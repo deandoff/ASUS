6729a5b93f28c15e4404a075c63591a4ab0e50da
bagaart <artemka.bagautdinoff1@yandex.ru>
2024-11-25 16:00:07 +0300

    auth_window style added

--- ui/auth_window.py
+++ ui/auth_window.py
@@ -1,6 +1,6 @@
 from PySide6.QtWidgets import QApplication, QMainWindow, QLabel, QLineEdit, QPushButton, QVBoxLayout, QWidget, \
-    QSpacerItem, QSizePolicy, QMessageBox
-from PySide6.QtCore import Qt, QRect
+    QMessageBox
+from PySide6.QtCore import Qt
 
 from ui.main_window import MainAppWindow
 
@@ -10,44 +10,76 @@ class LoginWindow(QMainWindow):
         super().__init__()
 
         self.setWindowTitle("ASUS Authorization")
-        self.setFixedSize(500,300)
+        self.setFixedSize(500, 300)
 
         # Центральный виджет
         self.central_widget = QWidget()
         self.setCentralWidget(self.central_widget)
+        self.central_widget.setStyleSheet('''background-color: #F0FFFF;''')
+
+        # Заголовок "Вход"
+        self.label_title = QLabel("Вход")
+        self.label_title.setAlignment(Qt.AlignCenter)
+        self.label_title.setStyleSheet('''font-family: Roboto Slab; 
+                                            font-size: 24px; 
+                                            font-weight: bold; 
+                                            color: black; 
+                                            margin-bottom: 20px;''')
 
         # Создание меток и полей ввода
-        self.label_username = QLabel()
+        self.label_username = QLabel("Логин")
         self.input_username = QLineEdit()
         self.input_username.setPlaceholderText("Введите логин")
-        self.input_username.setLayout(QVBoxLayout())
-        self.input_username.setFixedSize(480,40)
+        self.input_username.setFixedSize(300, 40)
         self.input_username.setAlignment(Qt.AlignCenter)
+        self.input_username.setStyleSheet('''font-family: Roboto Slab; 
+                                               font-size: 17px;
+                                               border: 2px solid black;
+                                               border-radius: 10px;
+                                               padding: 5px;
+                                               ''')
+        self.input_username.returnPressed.connect(self.focus_on_password)
 
-
-        self.label_password = QLabel()
+        self.label_password = QLabel("Пароль")
         self.input_password = QLineEdit()
         self.input_password.setPlaceholderText("Введите пароль")
         self.input_password.setEchoMode(QLineEdit.Password)
-        self.input_password.setFixedSize(480,40)
+        self.input_password.setFixedSize(300, 40)
         self.input_password.setAlignment(Qt.AlignCenter)
-
+        self.input_password.setStyleSheet('''font-family: Roboto Slab; 
+                                               font-size: 17px;
+                                               border: 2px solid black;
+                                               border-radius: 10px;
+                                               padding: 5px;
+                                               ''')
+        self.input_password.returnPressed.connect(self.handle_login)
 
         # Кнопка авторизации
         self.button_login = QPushButton("Авторизоваться")
         self.button_login.clicked.connect(self.handle_login)
-        self.button_login.setFixedSize(480,40)
+        self.button_login.setFixedSize(200, 40)
+        self.button_login.setStyleSheet('''font-family: Roboto Slab; 
+                                             font-size: 17px; 
+                                             color: white; 
+                                             background-color: black; 
+                                             border: 2px solid black; 
+                                             border-radius: 10px; 
+                                             padding: 5px; 
+                                             ''')
 
         # Компоновка
         layout = QVBoxLayout()
-        layout.setAlignment(Qt.AlignCenter)  # Центрирование всего содержимого
         layout.addStretch(1)
-        layout.addWidget(self.input_username)
-        layout.addWidget(self.input_password)
+        layout.addWidget(self.label_title, alignment=Qt.AlignCenter)  # Добавляем заголовок
+        layout.addWidget(self.input_username, alignment=Qt.AlignCenter)
+        layout.addWidget(self.input_password, alignment=Qt.AlignCenter)
         layout.addStretch(1)
-        layout.addWidget(self.button_login)
+        layout.addWidget(self.button_login, alignment=Qt.AlignCenter)
 
         self.central_widget.setLayout(layout)
+
+    def focus_on_password(self):
+        self.input_password.setFocus()
 
     def handle_login(self):
         username = self.input_username.text()
@@ -57,13 +89,36 @@ class LoginWindow(QMainWindow):
             self.close()  # Закрываем окно авторизации
             self.open_main_window()  # Открываем главное окно
         else:
-            QMessageBox.information(self,"Ошибка авторизации", "Неверный логин и/или пароль")
+            msg_box = QMessageBox()
+            msg_box.setWindowTitle("Ошибка авторизации")
+            msg_box.setText("Неверный логин и/или пароль")
+            msg_box.setStyleSheet('''
+                    QMessageBox {
+                        background-color: #F0FFFF;
+                        color: black;
+                        font-family: Roboto Slab;
+                        font-size: 16px;
+                    }
+                    QPushButton {
+                        background-color: black;
+                        color: white;
+                        border: 1px solid black;
+                        border-radius: 5px;
+                        padding: 5px;
+                    }
+                    QPushButton:hover {
+                        background-color: #555;
+                    }
+                ''')
+            msg_box.setStandardButtons(QMessageBox.Ok)
+            msg_box.exec()
             self.input_username.clear()
             self.input_password.clear()
 
     def open_main_window(self):
         self.main_window = MainAppWindow()
         self.main_window.show()
+
 
 if __name__ == "__main__":
     app = QApplication([])
